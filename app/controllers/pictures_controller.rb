53b6786d8c76defa0ddf810658466ef11145f3b7
Takuya <yky2800.rthm.55tn@gmail.com>
2018-10-21 17:36:55 +0900

    Instagram_clone make picture_update & partial

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update]
+
   def new
     @picture = Picture.new
   end
@@ -12,10 +14,17 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: '投稿しました！'
+    else
+      render 'edit'
+    end
   end
 
   def index
@@ -29,6 +38,10 @@ class PicturesController < ApplicationController
 
   def picture_params
     params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,4 @@
 class Picture < ApplicationRecord
-  validates :image, presence: true
   validates :content, length: { in: 1..140 }
   mount_uploader :image, ImageUploader
 end
