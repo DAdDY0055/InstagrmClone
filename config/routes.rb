177b420775c9f14125a621334083023c43c954c6
Takuya <yky2800.rthm.55tn@gmail.com>
2018-10-22 20:02:55 +0900

    InstagramClone make session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   resources :pictures do
     collection do
       post :confirm
     end
   end
+
   resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
