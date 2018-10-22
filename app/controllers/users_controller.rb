5b3ccbcc007bd293618f5d5ed2816681a3ef4a75
Takuya <yky2800.rthm.55tn@gmail.com>
2018-10-22 19:22:57 +0900

    InstagramClone add account_make_error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
