50ba035e914f134248e5b9a43fd5ccc2835bad5e
Takuya <yky2800.rthm.55tn@gmail.com>
2018-10-27 16:33:14 +0900

    InstagramClone modified profile add users_picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_pictures = Picture.where("user_id = ?", (params[:id]))
   end
 
   def favorite_index
