00965de0a6bdbd3089f4e07699cb23b1abb7d384
Takuya <yky2800.rthm.55tn@gmail.com>
2018-11-13 12:26:19 +0900

    change production.rb for send_mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '' }
+  config.action_mailer.default_url_options = { host: 'picture-sns.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password:  ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
